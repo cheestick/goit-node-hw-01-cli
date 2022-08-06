d21d07e77aa32aac4190b44e9e9a39498ae55f06
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-08-06 13:10:10 +0300

    node cli app inited

---
+++ contacts.js
@@ -0,0 +1,34 @@
+const fs = require('fs/promises')
+const path = require('path')
+
+const contactsPath = path.join(__dirname, 'db', 'contacts.json')
+console.log(contactsPath)
+
+/*
+ * Раскомментируй и запиши значение
+ * const contactsPath = ;
+ */
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+}
---
+++ index.js
@@ -0,0 +1,30 @@
+const contactsApi = require('./contacts')
+const argv = require('yargs').argv
+
+contactsApi.listContacts()
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      // ...
+      break
+
+    case 'get':
+      // ... id
+      break
+
+    case 'add':
+      // ... name email phone
+      break
+
+    case 'remove':
+      // ... id
+      break
+
+    default:
+      console.warn('\x1B[31m Unknown action type!')
+  }
+}
+
+invokeAction(argv)
